1cf34b19f66b7121d6da25043461d9aa8446e26e
Mahdi <mahdimey17@gmail.com>
2024-01-25 22:07:34 +0330

    added showTime function

--- script.js
+++ script.js
@@ -48,6 +48,22 @@ function showData(data) {
     temperature.innerHTML = `${(data.main.temp - 273.15).toFixed(1)}°c`
     degrees.innerHTML = `${(data.main.temp_min - 273.15).toFixed(1)}°c / ${(data.main.temp_max - 273.15).toFixed(1)}°c`
     weather.innerHTML = `${data.weather[0].main}`
+    dataElement.innerHTML = showTime()
+}
+
+function showTime(){
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let now = new Date
+
+    let day = days[now.getDay()]
+    let month = months[now.getMonth()]
+    let year = now.getFullYear()
+    let date = now.getDate()
+
+    return `${day} ${date} ${month} ${year}`
 }
 
 // a button for phone devices
